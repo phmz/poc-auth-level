1a75bafc33792a0b893ee9f2ad7cee15562dfc54
Maxime Petit <mpetithengen@gmail.com>
2023-11-09 23:14:48 +0100

    test: add e2e tests for user roles

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { AppService } from '../src/app.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -9,16 +10,50 @@ describe('AppController (e2e)', () => {
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      providers: [AppService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/GET user with role "admin"', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/user?role=admin')
       .expect(200)
-      .expect('Hello World!');
+      .then((response) => {
+        expect(response.body).toHaveProperty('id');
+        expect(response.body).toHaveProperty('username');
+        expect(response.body).toHaveProperty('email');
+        expect(response.body).toHaveProperty('role');
+      });
+  });
+
+  it('/GET user with role "user"', () => {
+    return request(app.getHttpServer())
+      .get('/user?role=user')
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toHaveProperty('id');
+        expect(response.body).toHaveProperty('username');
+        expect(response.body).toHaveProperty('email');
+        expect(response.body).not.toHaveProperty('role');
+      });
+  });
+
+  it('/GET user with role "support"', () => {
+    return request(app.getHttpServer())
+      .get('/user?role=support')
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toHaveProperty('id');
+        expect(response.body).toHaveProperty('username');
+        expect(response.body).not.toHaveProperty('email');
+        expect(response.body).toHaveProperty('role');
+      });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
